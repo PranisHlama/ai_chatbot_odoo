41f69d052d0ce91041441b15ca36db4833421936
Pranish Lama <pranishlama17@gmail.com>
2025-09-11 11:32:44 +0545

    feat: changed chatbot markdown format to plain text

--- gemini_chatbot/models/message_bot.py
+++ gemini_chatbot/models/message_bot.py
@@ -1,7 +1,16 @@
 from odoo import models, api
 import re, logging
+import markdown
+from bs4 import BeautifulSoup
 
 _logger = logging.getLogger(__name__)
+
+def strip_markdown(md_text_: str) -> str:
+    html = markdown.markdown(md_text_)
+
+    soup = BeautifulSoup(html, "html.parser")
+    text = soup.get_text(separator="\n")
+    return text.strip()
 
 class MailMessage(models.Model):
     _inherit = "mail.message"
@@ -12,6 +21,7 @@ class MailMessage(models.Model):
 
         messages = super().create(vals_list)
         bot_partner = self.env['res.partner'].browse(96)
+
         for message in messages:
             if message.model == "discuss.channel" and message.res_id:
                 channel = self.env["discuss.channel"].browse(message.res_id)
@@ -31,10 +41,11 @@ class MailMessage(models.Model):
 
                 try:
                     reply = self.env["gemini.chatbot.api"].ask_gemini(text)
-                    _logger.info(f"[GeminiBot] Gemini replied: {reply}")
+                    clean_reply = strip_markdown(reply)
+                    _logger.info(f"[GeminiBot] Gemini replied: {clean_reply}")
 
                     channel.message_post(
-                        body=f" {reply}",
+                        body= clean_reply,
                         message_type="comment",
                         subtype_xmlid="mail.mt_comment",
                         author_id=bot_partner.id,
@@ -48,3 +59,4 @@ class MailMessage(models.Model):
                     )
 
         return messages
+
