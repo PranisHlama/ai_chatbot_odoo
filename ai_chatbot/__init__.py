e164d80835b9a5a150e7da1d5b05edd2e7d2e458
PranisHlama <pranishlama17@gmail.com>
2025-11-30 18:26:20 +0000

    feat: strip down everything

--- ai_chatbot/__init__.py
+++ ai_chatbot/__init__.py
@@ -1,5 +1,1 @@
-# -*- coding: utf-8 -*-
-
-from . import hooks
 from . import models
-from .hooks import create_discuss_channel
--- ai_chatbot/__manifest__.py
+++ ai_chatbot/__manifest__.py
@@ -1,10 +1,6 @@
 {
     'name': "AI Chatbot",
     'summary': "Add AI chatbot functionality directly to Odoo discuss channels",
-    'description': """
-    AI Chatbot for Discuss Channels
-    This module adds a simple AI chatbot directly to your Odoo discuss channels.
-    """,
     'author': "Pranish Lama",
     'category': 'Discuss',
     'version': '1.0.0',
@@ -13,7 +9,6 @@
         # 'data/gemini_bot_data.xml',
         # 'views/views.xml',
     ],
-    # 'post_init_hook': 'create_discuss_channel',
     'installable': True,
     'application': False,
     'auto_install': False,
